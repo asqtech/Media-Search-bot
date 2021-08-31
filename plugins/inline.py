452388864efe66abb5fff2b64ecadd99da58905e
Joey0070 <88881246+Joey0070@users.noreply.github.com>
2021-08-31 21:16:03 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Search again🔎', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('Update Channel', url='https://t.me/worldmovieszztech')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
