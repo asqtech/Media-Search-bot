6ab6d5b043484ad86de9639b15db2dabf5596e78
Joey0070 <88881246+Joey0070@users.noreply.github.com>
2021-08-31 21:14:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,8 +66,8 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+                        InlineKeyboardButton('Search again🔎', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('Update Channel', url='https://t.me/worldmovieszztech')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -99,8 +99,8 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Search Here🔎", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("Update Channel", url="https://t.me/worldmovieszztech")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
@@ -192,8 +192,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            InlineKeyboardButton('Update Channel', url='https://t.me/worldmovieszztech'),
+            InlineKeyboardButton('Source Code', url='https://t.me/sourcecodeveno')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Developer : <a href='https://t.me/DeepakV1111_bot'>JOEY</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://t.me/sourcecodeveno'>Click here</a>\nUpdate Channel : <a href='https://t.me/worldmovieszztech'>WM Projects</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
